ad55ed9526e0cfcbaea6d823df46c1177ad1b7f5
youyuanbo <ug.python.linux@gmail.com>
2019-03-21 22:02:18 +0800

    OOP_CPP

---
+++ cmake-build-debug/CMakeFiles/OOP_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_CPP.dir/main.cpp.obj"
+  "OOP_CPP.pdb"
+  "OOP_CPP.exe"
+  "OOP_CPP.exe.manifest"
+  "libOOP_CPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
